0577ee74f946a87da74a3364ff638148c570e455
USERNAME <pellysherlynn@gmail.com>
2026-07-06 09:29:14 +0800

    lab-4-01

---
+++ labs/lab04/exercise1/lab-4-1.py
@@ -0,0 +1,9 @@
+kWh = float(input())
+if kWh <= 100:
+    totalBill = 0 * 30 * kWh
+else:
+    if kWh <= 200:
+        totalBill = 100 * 0.3 + kWh - 100 * 0.5
+    else:
+        totalBill = 100 * 0.3 + 100 * 0.5 + kWh - 200 * 0.75
+print(totalBill)
