6b1373d0127ca805828a33e30ce88d512eecdb61
USERNAME <pellysherlynn@gmail.com>
2026-08-10 09:18:23 +0800

    labttest2

---
+++ labs/lab-test-2.py
@@ -0,0 +1,4 @@
+name = "Ahmad Bin Abu"
+matric = "Matric. No: MS2025123499"
+marks = "2x10 marks"
+print (f"Name: {name}\t\t\t\t\t\t{matric}\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\n\nThis is my\n\tsecond\n\t\tassignment\nI want {marks},which is 20 full marks")
