e05a441a662fb71fccadc45184a48b7cbe98ea6b
USERNAME <pellysherlynn@gmail.com>
2026-08-10 08:41:05 +0800

    06

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -9,5 +9,5 @@ Totalwater = 1.05 * 4
 Subtotal = Totalcoffee + Totalmuffin + Totalwater
 Tax = Subtotal * (6 / 100)
 Total = Subtotal + Tax
-RECEIPT = f"RECEIPT:\n\nItem\tPrice\tQty\tTotal\nCoffee\t$3.50\t2\t${Totalcoffee}\nMuffin\t$2.10\t3\t${Totalmuffin}\nWater\t${Totalwater}\t4\t$4.20\n\nSubtotal\t\t${Subtotal}\n\nTax\t\t\t${Tax}\n\nTotal\t\t\t${Total}"
+RECEIPT = f"============RECEIPT==========:\n\nItem\tPrice\tQty\tTotal\nCoffee\t$3.50\t2\t${Totalcoffee}\nMuffin\t$2.10\t3\t${Totalmuffin}\nWater\t${Totalwater}\t4\t$4.20\n\nSubtotal\t\t${Subtotal}\n\nTax\t\t\t${Tax}\n\nTotal\t\t\t${Total}"
 print(RECEIPT)
