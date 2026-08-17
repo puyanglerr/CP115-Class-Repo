d515783bc0fffb0fb7d76c81d1d7896d7d055281
USERNAME <pellysherlynn@gmail.com>
2026-08-17 09:06:24 +0800

    Complete Lab 7

--- labs/lab07/exercise.py
+++ labs/lab07/exercise.py
@@ -1,2 +1,8 @@
-# Lab 07 scratch file
-# Use this file to try out the code from the lab as you follow along.
+import math
+import random
+
+# Using the modules
+result = math.sqrt(25)
+number = random.randint(1, 10)
+print(float(result))
+print(number)
