610cf7c502713ef3ebdd0500aaecd0012b6cf334
USERNAME <pellysherlynn@gmail.com>
2026-09-21 09:21:55 +0800

    labtest3

---
+++ labs/lab-test-3.py
@@ -0,0 +1,9 @@
+monthly_usage = int(input("Enter monthly usage: "))
+if monthly_usage < 50:
+    amount_bill = monthly_usage
+elif monthly_usage <= 100:
+    amount_bill = monthly_usage - (monthly_usage * (5/100))
+else:
+    amount_bill = monthly_usage - (monthly_usage * (20/100))
+
+print(" Amount of the bill to be paid after receiving the discount: ", amount_bill)
