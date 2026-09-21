8c5ec5e1815c2fc1a1a342f6d7aac5284200f478
USERNAME <pellysherlynn@gmail.com>
2026-09-21 08:45:59 +0800

    exercise 2

--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -1,5 +1,13 @@
 num_days = int(input())
 danger_threshold = float(input())
+danger_days = 0
+total_temp = 0 
+average = 35.5
+for i in range(num_days):
+    if temnp >= danger_threshold:
+        danger_days = danger_days + 1
+
+    
 
 
 
